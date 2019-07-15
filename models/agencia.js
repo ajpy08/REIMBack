59a263f0998313a0a80b950ff623f79b458cafcc
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2019-07-15 10:53:42 -0500

    Cambios para catalogo de agencia

--- models/agencia.js
+++ models/agencia.js
@@ -7,34 +7,31 @@ var rolesValidos = {
     message: '{VALUE} no es un rol permitido'
 };
 
-
-var aaSchema = new Schema({
-    razonSocial: { type: String, unique: true, requiered: [true, 'La razon social es necesario'] },
-    nombreComercial: { type: String, requiered: [true, 'EL nombre comercial es necesaria'] },
+var agenciaSchema = new Schema({
     rfc: { type: String, unique: true, required: [true, 'El RFC es necesario'] },
-    calle: { type: String, requiered: [true, 'La calle es necesaria'] },
-    noExterior: { type: String, required: [true, 'El numero exterior es necesario'] },
+    razonSocial: { type: String, unique: true, requiered: [true, 'La razon social es necesario'] },
+    nombreComercial: { type: String, requiered: false },
+    calle: { type: String, requiered: false },
+    noExterior: { type: String, required: false },
     noInterior: { type: String, required: false },
-    colonia: { type: String, requiered: [true, 'La colonia es necesaria'] },
-    municipio: { type: String, requiered: [true, 'El Municio/Delegación es necesaria'] },
-    ciudad: { type: String, requiered: [true, 'La ciudad es necesaria'] },
-    estado: { type: String, requiered: [true, 'El estado es necesaria'] },
-    cp: { type: String, requiered: [true, 'El codigo postal es necesaria'] },
+    colonia: { type: String, requiered: false },
+    municipio: { type: String, requiered: false },
+    ciudad: { type: String, requiered: false },
+    estado: { type: String, requiered: [true, 'El estado es necesario'] },
+    cp: { type: String, requiered: [true, 'El codigo postal es necesario'] },
     formatoR1: { type: String, required: false },
-    correo: { type: String, requiered: [true, 'EL correo es necesaria'] },
-    correoFac: { type: String, requiered: [true, 'El correo de facturación es necesaria'] },
-    credito: { type: String, requiered: [true, 'EL credito es necesaria'] },
-    role: { type: String, required: true, default: 'AA_ROLE', enum: rolesValidos },
+    correo: { type: String, requiered: false },
+    correoFac: { type: String, requiered: false },
+    credito: { type: Boolean, requiered: [true, 'EL credito es necesario'], default: false },
     img: { type: String, required: false },
     patente: { type: String, requiered: [true, 'La patente es necesaria'] },
+    role: { type: String, required: true, default: 'AA_ROLE', enum: rolesValidos },
     usuarioAlta: { type: Schema.Types.ObjectId, ref: 'Usuario' },
     fAlta: { type: Date, default: Date.now },
     usuarioMod: { type: Schema.Types.ObjectId, ref: 'Usuario' },
     fMod: { type: Date }
-
-    
 }, { collection: 'clientes' });
 
-aaSchema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' })
+agenciaSchema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' })
 
-module.exports = mongoose.model('Agencia', aaSchema);
+module.exports = mongoose.model('Agencia', agenciaSchema);
