6b7e45fc00bf9f752e8e47ccba43e5eb784b9d39
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2019-07-24 10:32:16 -0500

    Se modifico para camiones y se creo archivo varias.js

--- public/varias.js
+++ public/varias.js
@@ -3,7 +3,7 @@ exports.ParamsToJSON = function ParamsToJSON(req) {
 
     for (var param in req.params) {
         if (req.params.hasOwnProperty(param)) {
-            if (req.params[param] != '' && req.params[param] != null && req.params[param] != undefined) {
+            if (req.params[param] != '' && req.params[param] != null && req.params[param] != 'undefined') {
                 filtro += '\"' + param + '\"' + ':' + '\"' + req.params[param] + '\"' + ',';
                 //console.log(param, req.params[param]);
             }
@@ -15,7 +15,7 @@ exports.ParamsToJSON = function ParamsToJSON(req) {
 
     var o = JSON.parse(filtro);
 
-    console.log(o);
+    //console.log(req.params);
 
     return o;
 }
