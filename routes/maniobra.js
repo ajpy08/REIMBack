0a0b227629284e513ff3715bfc2d0e657cc93b3e
Angel Puc <angelus_03@hotmail.com>
2019-08-03 02:32:32 -0500

    03AgostoCasa

--- routes/maniobra.js
+++ routes/maniobra.js
@@ -6,6 +6,11 @@ var mongoose = require('mongoose');
 var app = express();
 var Maniobra = require('../models/maniobra');
 
+var fileUpload = require('express-fileupload');
+var fs = require('fs');
+var uuid = require('uuid/v1');
+
+app.use(fileUpload());
 // ==========================================
 //  Obtener Maniobra por ID
 // ==========================================
@@ -309,6 +314,10 @@ app.delete('/:id', mdAutenticacion.verificaToken, (req, res) => {
     });
 });
 
+
+
+
+
 // ==========================================
 // Remover fotos lavado de la maniobra
 // ==========================================
@@ -382,5 +391,44 @@ app.put('/removeimgr/:id&:img', mdAutenticacion.verificaToken, (req, res) => {
 
 });
 
+// ==========================================
+// Subir fotos lavado de la maniobra
+// ==========================================
+app.put('/addimg/:id', (req, res, next) => {
+    var id = req.params.id;
+
+    if (!req.files) {
+        return res.status(400).json({
+            ok: false,
+            mensaje: 'No selecciono nada',
+            errors: { message: 'Debe de seleccionar una imagen' }
+        });
+    }
+
+    // Obtener nombre del archivo
+    var archivo = req.files.file;
+    var nombreCortado = archivo.name.split('.');
+    var extensionArchivo = nombreCortado[nombreCortado.length - 1];
+
+    var nombreArchivo = `${uuid()}.${extensionArchivo}`;
+    var path = './uploads/fotos_lavado/' + nombreArchivo;
+    archivo.mv(path, err => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al mover archivo',
+                errors: err
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            mensaje: 'Archivo guardado en tmp',
+            nombreArchivo: nombreArchivo,
+            path: path
+        });
+    });
+
+});
 // export
 module.exports = app;
