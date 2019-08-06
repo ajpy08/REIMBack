a0acee6181444778b51fb38574bd78cd35b22236
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2019-08-06 12:37:08 -0500

    Se hicieron modificaciones para que funcione recuperar contraseña se cambio temporalmente todo a locahost para funcionar.

--- config/configpass.js
+++ config/configpass.js
@@ -1,8 +1,8 @@
 var nodemailer = require('nodemailer'),
     hbs = require('nodemailer-express-handlebars'),
     path = require('path'),
-    email = process.env.MAILER_EMAIL_ID || 'emirpalm@gmail.com',
-    pass = process.env.MAILER_PASSWORD || 'boryitbsckzmaczr';
+    email = process.env.MAILER_EMAIL_ID || 'patiocontenedoresreim@gmail.com',
+    pass = process.env.MAILER_PASSWORD || 'fmat*0348';
 
 var smtpTransport = nodemailer.createTransport({
     service: process.env.MAILER_SERVICE_PROVIDER || 'Gmail',
