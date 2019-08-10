a1c8693a882b97b602e681408907b107c924ced3
Angel Puc <angelus_03@hotmail.com>
2019-08-09 19:04:13 -0500

    09AgostoOfVIAJES

--- models/viaje.js
+++ models/viaje.js
@@ -2,21 +2,29 @@ var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 var Maniobra = require('./maniobra');
-
+var moment = require('moment');
 
 var viajeSchema = new Schema({
-  anio: { type: Date, default: Date.now },
-  viaje: { type: String, unique: true, required: [true, 'El viaje es necesario'] },
-  buque: { type: Schema.Types.ObjectId, ref: 'Buque', required: true },
+  anio: { type: Number },
+  viaje: { type: String, required: [true, 'El viaje es necesario'] },
+  buque: { type: Schema.Types.ObjectId, ref: 'Buque', required: [true, 'El viaje es necesario'] },
   naviera: { type: Schema.Types.ObjectId, ref: 'Naviera' },
-  fArribo: { type: Date, default: Date.now },
-  fVigenciaTemporal: { type: Date, default: Date.now },
+  fArribo: { type: Date, default: moment().startOf('day') },
+  fVigenciaTemporal: { type: Date, default: moment().startOf('day') },
   contenedores: { type: [] },
   pdfTemporal: { type: String, required: false },
   usuarioAlta: { type: Schema.Types.ObjectId, ref: 'Usuario' },
-  fAlta: { type: Date, default: Date.now },
+  fAlta: { type: Date, default: new Date() },
   usuarioMod: { type: Schema.Types.ObjectId, ref: 'Usuario' },
   fMod: { type: Date },
 }, { collection: 'viajes' });
+
+viajeSchema.pre(['remove'], function(next) {
+  Maniobra.remove({ 'viaje': this._id }).exec();
+  next();
+});
+
+viajeSchema.index({ anio: 1, viaje: 1, buque: 1 }, { unique: true });
 viajeSchema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' })
+
 module.exports = mongoose.model('Viaje', viajeSchema);
