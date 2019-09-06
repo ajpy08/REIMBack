d2c788c19861764a02e70856e0b34f9efb9f1a45
Angel Puc <angelus_03@hotmail.com>
2019-09-06 18:42:13 -0500

    listoUsuariosReparacionesProfile

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,7 +5,7 @@ var bcrypt = require('bcryptjs');
 var fs = require('fs');
 var jwt = require('jsonwebtoken');
 var Usuario = require('../models/usuario');
-
+var varias = require('../public/varias');
 // Inicializar variables
 var app = express();
 
@@ -59,12 +59,39 @@ app.get('/usuario/:id', (req, res) => {
     });
 });
 
+// ==========================================
+//  Obtener usuario por ID
+// ==========================================
+app.get('/usuario/:id/includes', (req, res) => {
+  var id = req.params.id;
+  Usuario.findById(id)
+    .populate('empresas', 'razonSocial')
+    .exec((err, usuario) => {
+      if (err) {
+        return res.status(500).json({
+          ok: false,
+          mensaje: 'Error al buscar usuario',
+          errors: err
+        });
+      }
+      if (!usuario) {
+        return res.status(400).json({
+          ok: false,
+          mensaje: 'La usuario con el id ' + id + 'no existe',
+          errors: { message: 'No existe un usuario con ese ID' }
+        });
+      }
+      res.status(200).json({
+        usuario: usuario
+      });
+    });
+});
 // =======================================
 // Crear Usuarios
 // =======================================
 
 
-app.post('/', mdAutenticacion.verificaToken, (req, res) => {
+app.post('/usuario', mdAutenticacion.verificaToken, (req, res) => {
   var body = req.body;
   var usuario = new Usuario({
     nombre: body.nombre,
@@ -73,15 +100,16 @@ app.post('/', mdAutenticacion.verificaToken, (req, res) => {
     role: body.role,
     empresas: body.empresas,
     img: body.img,
+    observaciones: body.observaciones,
     usuarioAlta: req.usuario._id
   });
 
-  if (fs.existsSync('./uploads/temp/' + usuario.img)) {
-    console.log('The file exists.');
-    fs.rename('./uploads/temp/' + usuario.img, './uploads/usuarios/' + usuario.img, (err) => {
-      if (err) { console.log(err); }
-    });
+  if (usuario.empresas.length <= 0) {
+    usuario.empresas = undefined;
   }
+
+  varias.MoverArchivoFromTemp('./uploads/temp/', usuario.img, './uploads/usuarios/', usuario.img);
+
   usuario.save((err, usuarioGuardado) => {
     if (err) {
       return res.status(400).json({
@@ -103,7 +131,7 @@ app.post('/', mdAutenticacion.verificaToken, (req, res) => {
 // =======================================
 // Actualizar Usuarios
 // =======================================
-app.put('/:id', [mdAutenticacion.verificaToken, mdAutenticacion.verificaADMIN_o_MismoUsuario], (req, res) => {
+app.put('/usuario/:id', [mdAutenticacion.verificaToken, mdAutenticacion.verificaADMIN_o_MismoUsuario], (req, res) => {
   var id = req.params.id;
   var body = req.body;
   Usuario.findById(id, (err, usuario) => {
@@ -121,30 +149,21 @@ app.put('/:id', [mdAutenticacion.verificaToken, mdAutenticacion.verificaADMIN_o_
         errors: { message: 'No existe un usuario con ese ID' }
       });
     }
-
     usuario.nombre = body.nombre;
     usuario.email = body.email;
-    //usuario.role = body.role;
-    //usuario.password = body.password;
-    if (body.empresas)
+    if (body.empresas && body.empresas.length > 0) {
       usuario.empresas = body.empresas;
+    } else {
+      usuario.empresas = undefined;
+    }
+    usuario.observaciones = body.observaciones;
     usuario.usuarioMod = req.usuario._id;
     usuario.fMod = new Date();
 
     if (usuario.img != body.img) {
-      if (fs.existsSync('./uploads/temp/' + body.img)) {
-        if (usuario.img != undefined || usuario.img != '' && usuario.img != null && fs.existsSync('./uploads/usuarios/' + usuario.img)) {
-          fs.unlink('./uploads/usuarios/' + usuario.img, (err) => {
-            if (err) console.log(err);
-            else
-              console.log('path/file.txt was deleted');
-          });
-        }
-        fs.rename('./uploads/temp/' + body.img, './uploads/usuarios/' + body.img, (err) => {
-          if (err) { console.log(err); }
-        });
+      if (varias.MoverArchivoFromTemp('./uploads/temp/', body.img, './uploads/usuarios/', usuario.img)) {
+        usuario.img = body.img;
       }
-      usuario.img = body.img;
     }
     usuario.save((err, usuarioGuardado) => {
       if (err) {
@@ -164,16 +183,13 @@ app.put('/:id', [mdAutenticacion.verificaToken, mdAutenticacion.verificaADMIN_o_
   });
 });
 
-
 // =======================================
 // Actualizar Usuarios
+//Al ser perfil solo se modifica , nombre, correo y fotografia si es necesario..
 // =======================================
-app.put('/usuario/:id/habilita_deshabilita', [mdAutenticacion.verificaToken, mdAutenticacion.verificaADMIN_o_MismoUsuario], (req, res) => {
+app.put('/usuario/:id/perfil', [mdAutenticacion.verificaToken, mdAutenticacion.verificaADMIN_o_MismoUsuario], (req, res) => {
   var id = req.params.id;
   var body = req.body;
-  console.log('habilitadeshabilita');
-  console.log(body);
-  console.log(id);
   Usuario.findById(id, (err, usuario) => {
     if (err) {
       return res.status(500).json({
@@ -189,10 +205,56 @@ app.put('/usuario/:id/habilita_deshabilita', [mdAutenticacion.verificaToken, mdA
         errors: { message: 'No existe un usuario con ese ID' }
       });
     }
+    usuario.nombre = body.nombre;
+    usuario.email = body.email;
+    usuario.usuarioMod = req.usuario._id;
+    usuario.fMod = new Date();
 
+    if (usuario.img != body.img) {
+      if (varias.MoverArchivoFromTemp('./uploads/temp/', body.img, './uploads/usuarios/', usuario.img)) {
+        usuario.img = body.img;
+      }
+    }
+    usuario.save((err, usuarioGuardado) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          mensaje: 'Error al actualizar perfil',
+          errors: err
+        });
+      }
+      usuarioGuardado.password = '=)';
+      res.status(200).json({
+        ok: true,
+        mensaje: 'Perfil Actualizado con éxito',
+        usuario: usuarioGuardado
+      });
+    });
+  });
+});
 
+// =======================================
+// Actualizar Usuarios
+// =======================================
+app.put('/usuario/:id/habilita_deshabilita', [mdAutenticacion.verificaToken, mdAutenticacion.verificaADMIN_o_MismoUsuario], (req, res) => {
+  var id = req.params.id;
+  var body = req.body;
+  Usuario.findById(id, (err, usuario) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: 'Error al buscar usuario',
+        errors: err
+      });
+    }
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: 'El usuario con el id ' + id + ' no existe',
+        errors: { message: 'No existe un usuario con ese ID' }
+      });
+    }
     usuario.activo = body.activo;
-
     usuario.save((err, usuarioGuardado) => {
       if (err) {
         return res.status(400).json({
