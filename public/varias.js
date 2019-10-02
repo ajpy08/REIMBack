284cad1e6ab11155ab4d2e14e759aee98a28f4db
Angel Puc <angelus_03@hotmail.com>
2019-10-02 16:39:38 -0500

    bucket

--- public/varias.js
+++ public/varias.js
@@ -1,5 +1,21 @@
-var fs = require('fs');
-var path = require('path');
+//var fs = require('fs');
+//var path = require('path');
+
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+
+//cconfigurar AWS con las claves de acceso
+AWS.config.update({
+  accessKeyId: "AKIAIZJVVYGLOPMWJ3BQ",
+  secretAccessKey: "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",
+  region: 'us-east-1'
+});
+
+var s3 = new AWS.S3();
+
+
+
 
 exports.ParamsToJSON = function ParamsToJSON(req) {
   var json;
@@ -38,23 +54,66 @@ exports.ParamsToJSON = function ParamsToJSON(req) {
   return json;
 }
 
+// exports.MoverArchivoFromTemp = function MoverArchivoFromTemp(rutaTmp, nametmp, rutaDestino, nameActual) {
+//   if (nametmp != null && nametmp != undefined && nametmp != '' && fs.existsSync(rutaTmp + nametmp)) {
+//     if (nameActual != null && nameActual != undefined && nameActual != '' && fs.existsSync(rutaDestino + nameActual)) {
+//       fs.unlink(rutaDestino + nameActual, (err) => {
+//         if (err) {
+//           // console.log(err);
+//         } else {
+//           // console.log('Documento anterior borrado con éxito');
+//         }
+//       });
+//     }
+//     if (!fs.existsSync(rutaDestino)) { // CHECAMOS SI EXISTE LA CARPETA CORRESPONDIENTE.. SI NO, LO CREAMOS.
+//       fs.mkdirSync(rutaDestino);
+//     }
+//     fs.rename(rutaTmp + nametmp, rutaDestino + nametmp, (err) => {
+//       if (err) { console.log(err); throw err; }
+//     });
+//     return (true);
+//   }
+//   return false;
+// }
+
+
 exports.MoverArchivoFromTemp = function MoverArchivoFromTemp(rutaTmp, nametmp, rutaDestino, nameActual) {
   if (nametmp != null && nametmp != undefined && nametmp != '' && fs.existsSync(rutaTmp + nametmp)) {
-    if (nameActual != null && nameActual != undefined && nameActual != '' && fs.existsSync(rutaDestino + nameActual)) {
-      fs.unlink(rutaDestino + nameActual, (err) => {
-        if (err) {
-          // console.log(err);
-        } else {
-          // console.log('Documento anterior borrado con éxito');
-        }
-      });
-    }
-    if (!fs.existsSync(rutaDestino)) { // CHECAMOS SI EXISTE LA CARPETA CORRESPONDIENTE.. SI NO, LO CREAMOS.
-      fs.mkdirSync(rutaDestino);
-    }
-    fs.rename(rutaTmp + nametmp, rutaDestino + nametmp, (err) => {
-      if (err) { console.log(err); throw err; }
+    // if (nameActual != null && nameActual != undefined && nameActual != '' && fs.existsSync(rutaDestino + nameActual)) {
+    //   fs.unlink(rutaDestino + nameActual, (err) => {
+    //     if (err) {
+    //       // console.log(err);
+    //     } else {
+    //       // console.log('Documento anterior borrado con éxito');
+    //     }
+    //   });
+    // }
+    // if (!fs.existsSync(rutaDestino)) { // CHECAMOS SI EXISTE LA CARPETA CORRESPONDIENTE.. SI NO, LO CREAMOS.
+    //   fs.mkdirSync(rutaDestino);
+    // }
+    // fs.rename(rutaTmp + nametmp, rutaDestino + nametmp, (err) => {
+    //   if (err) { console.log(err); throw err; }
+    // });
+
+    //configuring parameters
+    var params = {
+      Bucket: 'bucketcontainerpark',
+      Body: fs.createReadStream(rutaTmp + nametmp),
+      Key: rutaDestino + nametmp
+    };
+
+    s3.upload(params, function(err, data) {
+      //handle error
+      if (err) {
+        console.log("Error", err);
+      }
+
+      //success
+      if (data) {
+        console.log("Uploaded in:", data.Location);
+      }
     });
+
     return (true);
   }
   return false;
--- routes/imagenes.js
+++ routes/imagenes.js
@@ -8,72 +8,93 @@ var app = express();
 var path = require('path');
 var fs = require('fs');
 
+const AWS = require('aws-sdk');
+
+
+var s3 = new AWS.S3();
+
+
 // Rutas
 app.get('/:tipo/:img', (req, res, netx) => {
 
-    var tipo = req.params.tipo;
-    var img = req.params.img;
+  var tipo = req.params.tipo;
+  var img = req.params.img;
 
-    var pathImagen = path.resolve(__dirname, `../uploads/${tipo}/${img}`);
+  var pathImagen = path.resolve(__dirname, `../uploads/${tipo}/${img}`);
 
-    if (fs.existsSync(pathImagen)) {
-        res.sendFile(pathImagen);
-    } else {
-        var pathNoImagen = path.resolve(__dirname, `../assets/no-img.jpg`);
-        res.sendFile(pathNoImagen);
-    }
+  if (fs.existsSync(pathImagen)) {
+    res.sendFile(pathImagen);
+  } else {
+    var pathNoImagen = path.resolve(__dirname, `../assets/no-img.jpg`);
+    res.sendFile(pathNoImagen);
+  }
+
+
+  const params = {
+    Bucket: "bucketcontainerpark",
+    Key: `${tipo}/${img}`
+  };
+  s3.getObject(params, (err, data) => {
+    if (err) console.error(err);
+    fs.writeFileSync("", data.Body.toString());
+    console.log(`${filePath} has been created!`);
+  });
+
+
+
+
 });
 
 // Recupera Fotos de Maniobra (Lavado o Reparacion)
 app.get('/:id/:LR/:img', (req, res, netx) => {
-    var id = req.params.id;
-    var img = req.params.img;
-    var LR = req.params.LR;
+  var id = req.params.id;
+  var img = req.params.img;
+  var LR = req.params.LR;
 
-    var pathImagen = path.resolve(__dirname, `../uploads/maniobras/${id}/${LR}/${img}`);
-    if (fs.existsSync(pathImagen)) {
-        res.sendFile(pathImagen);
-    } else {
-        var pathNoImagen = path.resolve(__dirname, `../assets/no-img.jpg`);
-        res.sendFile(pathNoImagen);
-    }
+  var pathImagen = path.resolve(__dirname, `../uploads/maniobras/${id}/${LR}/${img}`);
+  if (fs.existsSync(pathImagen)) {
+    res.sendFile(pathImagen);
+  } else {
+    var pathNoImagen = path.resolve(__dirname, `../assets/no-img.jpg`);
+    res.sendFile(pathNoImagen);
+  }
 });
 
 // Fotos Lavados y Reparaciones de la carpeta
 app.get('/:maniobra&:LR', (req, res, netx) => {
-    var idManiobra = req.params.maniobra;
-    var lavado_reparacion = req.params.LR;
-    var pathFotos = "";
-    var array = [];
+  var idManiobra = req.params.maniobra;
+  var lavado_reparacion = req.params.LR;
+  var pathFotos = "";
+  var array = [];
 
-    pathFotos = varias.DevuelveRutaFotosLR(idManiobra, lavado_reparacion);
+  pathFotos = varias.DevuelveRutaFotosLR(idManiobra, lavado_reparacion);
 
-    if (pathFotos !== "" && fs.existsSync(pathFotos)) {
-        fs.readdir(pathFotos, function (err, files) {
-            var allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
-            var a = files.filter(function (file) { return allowedExtensions.exec(file); })
-            a.forEach(foto => {
-                var data = {
-                    name: foto,
-                    source: pathFotos + '\\' + foto
-                };
-                array.push(data)
-            });
+  if (pathFotos !== "" && fs.existsSync(pathFotos)) {
+    fs.readdir(pathFotos, function(err, files) {
+      var allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
+      var a = files.filter(function(file) { return allowedExtensions.exec(file); })
+      a.forEach(foto => {
+        var data = {
+          name: foto,
+          source: pathFotos + '\\' + foto
+        };
+        array.push(data)
+      });
 
-            res.status(200).json({
-                ok: true,
-                fotos: JSON.parse(JSON.stringify(array)),
-                total: array.length
-            });
-            // res.json(array)
-        });
-    } else {
-        // return res.status(400).json({
-        //     ok: false,
-        //     mensaje: 'No se encontraron fotos',
-        //     errors: { message: 'No existen fotos para ' + lavado_reparacion }
-        // });
-    }
+      res.status(200).json({
+        ok: true,
+        fotos: JSON.parse(JSON.stringify(array)),
+        total: array.length
+      });
+      // res.json(array)
+    });
+  } else {
+    // return res.status(400).json({
+    //     ok: false,
+    //     mensaje: 'No se encontraron fotos',
+    //     errors: { message: 'No existen fotos para ' + lavado_reparacion }
+    // });
+  }
 });
 
 // export
