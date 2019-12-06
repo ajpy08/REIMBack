55e21e7b0984bfde9b60480cdb51f62d119c19d9
RicGH <34562464+RicGH@users.noreply.github.com>
2019-12-06 13:53:25 -0600

    termino bl Booking y usuario Patio Rol

--- routes/login.js
+++ routes/login.js
@@ -264,7 +264,7 @@ function obtenerMenu(ROLE) {
     ];
     menu[0].submenu.unshift({ titulo: 'Lavado / Reparación', url: '/contenedoresLR' });
     menu[0].submenu.unshift({ titulo: 'Inventario', url: '/inventario' });
-    menu[0].submenu.unshift({ titulo: 'Maniobras Diario', url: '/maniobras_diario' });
+    menu[0].submenu.unshift({ titulo: 'Maniobras Patio', url: '/maniobras' });
     return menu;
   }
 
