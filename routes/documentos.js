f2c71fe1ef10346390d70d6132b385e7838848a2
RicGH <34562464+RicGH@users.noreply.github.com>
2020-02-28 17:11:12 -0600

    FALTA TERMINAR EL ZIP

--- routes/documentos.js
+++ routes/documentos.js
@@ -4,6 +4,10 @@ var app = express();
 var path = require('path');
 var AWS = require('aws-sdk');
 var variasBucket = require('../public/variasBucket');
+var s3Zip = require('s3-zip');
+var XmlStream = require('xml-stream');
+const fs = require('fs')
+const join = require('path').join
 
 app.get('/documentoxtipo/:tipo/:nombre', (req, res, netx) => {
   var tipo = req.params.tipo;
@@ -54,6 +58,140 @@ app.get('/maniobra/lavado_reparacion', (req, res, netx) => {
     });
   }
 });
+
+
+//DESCARGAR TODO LA CARPETA DE IMAGENES COMPRIMIDA //
+
+app.get('/maniobra/:id/zipLR/:LR/', (req, res, netx) => {
+  var idManiobra = req.params.id;
+  var lavado_reparacion = req.params.LR;
+  var pathFotos = "";
+  var carpeta = "";
+
+  if(lavado_reparacion === 'L'){
+    pathFotos = `maniobras/${idManiobra}/fotos_lavado/`;
+
+  } else {
+    if(lavado_reparacion === 'R') {
+      pathFotos = `maniobras/${idManiobra}/fotos_reparacion/`;
+    }
+  }
+  var s3 = new AWS.S3(entorno.CONFIG_BUCKET, {region: entorno.CONFIG_BUCKET.region});
+  var region = entorno.CONFIG_BUCKET.region;
+  var bucket = entorno.BUCKET;
+
+  var params = {
+    Bucket: entorno.BUCKET,
+    Prefix: pathFotos,
+  }
+  var filesArray = []
+  var files = s3.listObjects(params).createReadStream()
+  var xml = new XmlStream(files)
+  xml.collect('Key')
+  xml.on('endElement: Key', function(item) {
+    if(lavado_reparacion === 'L'){
+      carpeta = '/fotos_lavado/'
+    }else {
+      carpeta = '/fotos_reparacion/'
+    }
+    
+    filesArray.push(item['$text'].substr(carpeta.length -1))
+
+  })
+
+  xml.on('end', function(){
+    zip(filesArray)
+
+  })
+
+  function zip(files){
+    console.log(files)
+    var output = fs.createWriteStream(join(__dirname, 'prueba.zip'))
+    
+    s3Zip.archive({region: region, bucket: bucket}, carpeta, files).pipe(output)
+
+      
+
+
+  }
+
+  // s3Zip.archive(params, function(err, data){
+  //   if(err) {
+  //     return res.status(400).json({
+  //       ok: false,
+  //       mensaje: 'Error de descarga',
+  //       errors: err
+  //     });
+  //   } else {
+  //     return res.status(200).json({
+  //       ok: true,
+  //       mensaje: 'Vamos Bien',
+
+  //   })
+  //     console.log('Vamos Bien');
+  //   }
+  // })
+})
+
+
+  // ESTE ES EL BUNO HASRA EL MOMENTO LO DEJO PARA REFERENCIA, ELIMINAR CUANDO EL DE ARRIBA ESTE BIEN///
+  //
+// app.get('/maniobra/:id/zipLR/:LR/', (req, res, netx) => {
+//   var idManiobra = req.params.id;
+//   var lavado_reparacion = req.params.LR;
+//   var pathFotos = "";
+
+//   if(lavado_reparacion === 'L'){
+//     pathFotos = `maniobras/${idManiobra}/fotos_lavado/`;
+
+//   } else {
+//     if(lavado_reparacion === 'R') {
+//       pathFotos = `maniobras/${idManiobra}/fotos_reparacion/`;
+//     }
+//   }
+//   var s3 = new AWS.S3(entorno.CONFIG_BUCKET);
+//   const region = entorno.CONFIG_BUCKET.region;
+//   const bucket = entorno.BUCKET;
+
+//   var params = {
+//     Bucket: entorno.BUCKET,
+//     Prefix: pathFotos,
+//   }
+//   const filesArray = []
+//   const files = s3.listObjects(params).createReadStream()
+//   const xml = new XmlStream(files)
+//   xml.collect('Key')
+//   xml.on('endElement: Key', function(item) {
+//     filesArray.push(item['$text'].substr(bucket.length))
+//   })
+
+//   xml.on('end', function(){
+//     zip(filesArray)
+//   })
+
+//   function zip(files){
+//     console.log(files)
+//     const output = fs.createWriteStream(join(__dirname, 'zip.zip'))
+    
+//       s3Zip.archive({region: region, bucket: bucket, preserveFolderStructure: true}, Prefix, files)
+//       .pipe(output, res) 
+
+        
+//           // if(err) {
+//           //   return res.status(400).json({
+//           //     ok:false,
+//           //     mensaje: 'NO SE PUDO DESCARGAR EL ARCHIVO ZIP',
+//           //     errors: err
+//           //   });
+//           // } else {
+//           //   return res.status(200).json({
+//           //     ok: true,
+//           //     mensaje: 'Se descargo archivo ZIP',
+//           //     fotos: data.Contents 
+//           //   })
+//           // }
+//         }
+//       });
 
 
 // Fotos Lavados y Reparaciones de la carpeta
